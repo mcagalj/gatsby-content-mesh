3be58f4641fa031b1d5bef9cfa4349b950f5f23e
mcagalj <mario.cagalj@fesb.hr>
2020-10-29 17:08:17 +0100

    Add json content

--- src/pages/local.js
+++ src/pages/local.js
@@ -1,10 +1,35 @@
 import React from "react"
 import Layout from "../components/layout"
 
-const LocalPage = () => (
-  <Layout>
-    <h1>Local content</h1>
-  </Layout>
-)
+const LocalPage = ({ data }) => {
+  const { nodes: jsonData } = data.json
+  return (
+    <Layout>
+      <h1>Local content</h1>
+      <h2>Content from JSON files</h2>
+      <ol>
+        {jsonData.map(item => {
+          return (
+            <li>
+              {item.message} -- {item.author}
+            </li>
+          )
+        })}
+      </ol>
+    </Layout>
+  )
+}
 
 export default LocalPage
+
+export const query = graphql`
+  {
+    json: allJsonJson {
+      nodes {
+        id
+        author
+        message
+      }
+    }
+  }
+`
