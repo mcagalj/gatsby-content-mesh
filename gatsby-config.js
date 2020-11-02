d28500f33c67ac42a50baa69c9d3fb9956729cc9
mcagalj <mario.cagalj@fesb.hr>
2020-11-02 16:51:06 +0100

    Add Google Sheet

--- gatsby-config.js
+++ gatsby-config.js
@@ -110,6 +110,26 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
+    {
+      resolve: "gatsby-source-google-spreadsheet",
+      options: {
+        spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID,
+        credentials: {
+          type: "service_account",
+          project_id: process.env.PROJECT_ID,
+          private_key_id: process.env.PRIVATE_KEY_ID,
+          private_key: process.env.PRIVATE_KEY.replace(/(\\r)|(\\n)/g, "\n"),
+          client_email: process.env.CLIENT_EMAIL,
+          client_id: process.env.CLIENT_ID,
+          auth_uri: "https://accounts.google.com/o/oauth2/auth",
+          token_uri: "https://oauth2.googleapis.com/token",
+          auth_provider_x509_cert_url:
+            "https://www.googleapis.com/oauth2/v1/certs",
+          client_x509_cert_url:
+            "https://www.googleapis.com/robot/v1/metadata/x509/cagalj-4-google-sheets%40quickstart-1594282856586.iam.gserviceaccount.com",
+        },
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/pages/google-sheet.js
+++ src/pages/google-sheet.js
@@ -3,12 +3,51 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Title from "../components/title"
 
-const GoogleSheetPage = ({}) => {
+const GoogleSheetPage = ({ data }) => {
+  const { nodes: articles } = data.articles
   return (
     <Layout>
       <Title>Content from remote source (Google Sheet)</Title>
+      {articles.map(article => {
+        return (
+          <div key={article.id} className="mb-12">
+            <a href={article.url} className="p-1 pl-0">
+              {article.title}
+            </a>
+            <div>{article.author}</div>
+            <div className="flex">
+              <div>
+                {article.conference ? article.conference : article.journal},{" "}
+                {article.date}
+              </div>
+              {article.selected === "TRUE" ? (
+                <div className="p-0 ml-2 text-center text-xs w-1/12 text-white bg-purple-700 rounded-sm overflow-hidden">
+                  proud of
+                </div>
+              ) : null}
+            </div>
+          </div>
+        )
+      })}
     </Layout>
   )
 }
 
 export default GoogleSheetPage
+
+export const query = graphql`
+  {
+    articles: allGoogleSpreadsheetArticles {
+      nodes {
+        author
+        conference
+        date
+        id
+        journal
+        selected
+        title
+        url
+      }
+    }
+  }
+`
